e7d3328ffb10aafabf6f539aaadf74ab0d73f629
qingfeng-1 <75032805+qingfeng-1@users.noreply.github.com>
2026-03-18 19:19:08 +0800

    Fix formatting of custom feed source entry

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-echo 'src-git custom https://github.com/fichenx/openwrt-package.git ' >>feeds.conf.default
+echo 'src-git custom https://github.com/fichenx/openwrt-package.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
